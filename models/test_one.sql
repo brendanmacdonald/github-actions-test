2b7ce9f975e9e12d1269173573489aa1e4b0be03
Brendan MacDonald <macbrendan@gmail.com>
2025-11-20 16:56:14 +0000

    Another commit

--- models/test_one.sql
+++ models/test_one.sql
@@ -1,15 +1,16 @@
--- models/fake_avg_premium_order.sql
-
-WITH stg_main AS (
+with stg_main as (
   SELECT customer_id, order_total
-  FROM {{ source('sales', 'orders') }}
+  from {{ source('sales', 'orders') }}
 ),
 
-main AS (
-  SELECT *
-  FROM stg_main
-  WHERE customer_tier = 'Premium'
+main as (
+  select 
+    SUM(a)::int    as new_name,
+    b::INT         as NEW_name_2,
+    NULL::string   as no_value
+  from stg_main
+  where customer_tier = 'Premium'
 )
 
-SELECT *
+select *
 from main
